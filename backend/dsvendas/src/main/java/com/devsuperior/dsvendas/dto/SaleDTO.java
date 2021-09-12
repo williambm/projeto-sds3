ddeaa9e85708672aa0907199ace920d5549b28e2
William B. Montini <williambm@gmail.com>
2021-09-12 10:13:48 -0300

    Pagination

---
+++ backend/dsvendas/src/main/java/com/devsuperior/dsvendas/dto/SaleDTO.java
@@ -0,0 +1,86 @@
+package com.devsuperior.dsvendas.dto;
+
+import com.devsuperior.dsvendas.entities.Sale;
+
+import java.time.LocalDate;
+
+public class SaleDTO {
+
+    private Long id;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+
+    private SellerDTO sellerDTO;
+
+    public SaleDTO() {
+    }
+
+    public SaleDTO(Long id, Integer visited, Integer deals, Double amount, LocalDate date, SellerDTO sellerDTO) {
+        this.id = id;
+        this.visited = visited;
+        this.deals = deals;
+        this.amount = amount;
+        this.date = date;
+        this.sellerDTO = sellerDTO;
+    }
+
+    public SaleDTO(Sale entity) {
+        this.id = entity.getId();
+        this.visited = entity.getVisited();
+        this.deals = entity.getDeals();
+        this.amount = entity.getAmount();
+        this.date = entity.getDate();
+        //Aqui temos que criar um SellerDTO baseado na entidade Seller
+        this.sellerDTO = new SellerDTO(entity.getSeller());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getVisited() {
+        return visited;
+    }
+
+    public void setVisited(Integer visited) {
+        this.visited = visited;
+    }
+
+    public Integer getDeals() {
+        return deals;
+    }
+
+    public void setDeals(Integer deals) {
+        this.deals = deals;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public SellerDTO getSellerDTO() {
+        return sellerDTO;
+    }
+
+    public void setSellerDTO(SellerDTO sellerDTO) {
+        this.sellerDTO = sellerDTO;
+    }
+}
